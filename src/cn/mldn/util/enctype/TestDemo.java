fac432a3dd4c42cf9a2f0248feb1bd112eeab64d
ghy <1944462641@qq.com>
2018-08-16 17:37:22 +0800

    添加类

---
+++ src/cn/mldn/util/enctype/TestDemo.java
@@ -0,0 +1,14 @@
+package cn.mldn.util.enctype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestDemo {
+	public static void main(String[] args){
+		List<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("a");
+		System.err.println(list);
+	}
+
+}
