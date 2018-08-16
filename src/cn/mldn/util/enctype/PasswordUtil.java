8fd48c22a31f424679a5d64cbaaaa8d354ef1d70
ghy <1944462641@qq.com>
2018-08-16 17:39:29 +0800

    修改

--- src/cn/mldn/util/enctype/PasswordUtil.java
+++ src/cn/mldn/util/enctype/PasswordUtil.java
@@ -3,7 +3,7 @@ package cn.mldn.util.enctype;
 import java.util.Base64;
 
 public class PasswordUtil {
-	private static final String SEED  = "mldnjava" ;	// 该数据为种子数，如果要加密则需要使用Base64做多次迭代
+	private static final String SEED  = "GuoHongye" ;	// 该数据为种子数，如果要加密则需要使用Base64做多次迭代
 	private static final int NE_NUM = 3 ;	// 密码迭代处理3次
 	private PasswordUtil() {}
 	private static String createSeed() {	// 创建一个基于Base64的种子数
